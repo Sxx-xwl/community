4c48d728e560392929a05ace57a3e2cfb84e79c4
Sxx-xwl <sxxgryx27@163.com>
2022-04-22 20:12:16 +0800

    首次提交 mail

--- src/test/java/sxx/xwl/community/community/MailTest.java
+++ src/test/java/sxx/xwl/community/community/MailTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import sxx.xwl.community.community.util.MailClient;
 
 /**
@@ -17,9 +19,23 @@ public class MailTest {
     @Autowired
     private MailClient mailClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
-    public void test(){
-        mailClient.sendMail("1905921852@qq.com","test","hello");
+    public void testTextMail() {
+        mailClient.sendMail("1905921852@qq.com", "test", "hello");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "CC");
+
+        String process = templateEngine.process("/mail/demo", context);
+        System.out.println(process);
+
+        mailClient.sendMail("1905921852@qq.com", "welcome", process);
     }
 
 }
